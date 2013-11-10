2042b27b13ecfc456bd0f8c9b7cf0af65ce1f109
Da Wei <xichuanglian@gmail.com>
2013-11-10 19:36:32 +0800

    fixed missing line terminator bug

--- app/helpers/evaluation_forms_helper.rb
+++ app/helpers/evaluation_forms_helper.rb
@@ -1,5 +1,11 @@
 module EvaluationFormsHelper
   def field_name(symbol)
-    symbol.to_s.sub(/_/, ' ').capitalize
+    symbol.to_s.gsub(/_/, ' ').capitalize
+  end
+
+  def split_lines(text)
+    text.to_s.gsub(/[^\r\n]+/) do |line|
+      yield line
+    end
   end
 end
