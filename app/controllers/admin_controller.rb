d92a56c52e22749c3311cb8e82742c589ca63f04
Alan <alan43@aliyun.com>
2013-11-24 18:04:28 +0800

    user store teacher jobid

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,13 +10,13 @@ class AdminController < ApplicationController
     if user_params[:job] == 'Student'
       if advisor_to_id(user_params) == nil
         @check_advisor[:advisor] = 'error'
-        render admin_addnew_path
+        render 'new'
       elsif co_advisor_to_id(user_params) == nil
         @check_advisor[:co_advisor] = 'error'
-        render admin_addnew_path
+        render 'new'
       elsif official_advisor_to_id(user_params) == nil
         @check_advisor[:official_advisor] = 'error'
-        render admin_addnew_path
+        render 'new'
       else
         @user = User.create!(advisor_to_id(co_advisor_to_id(official_advisor_to_id(user_params))))
         flash[:notice] = 'Successfully added new user.'
@@ -34,18 +34,43 @@ class AdminController < ApplicationController
   end
 
   def edit
+    @check_advisor = Hash.new("")
     @user = User.find params[:id]
+    @user_advisor_name = User.find_by(jobid: @user.advisor).name
+    @user_co_advisor_name = User.find_by(jobid: @user.co_advisor).name
+    @user_official_advisor_name = User.find_by(jobid: @user.official_advisor).name
   end
 
   def update
+    @check_advisor = Hash.new("")
     @user = User.find params[:id]
-    @user.update_attributes!(user_params)
-    flash[:notice] = 'Successfully saved.'
-    redirect_to admin_showall_path
+    if user_params[:job] == 'Student'
+      if advisor_to_id(user_params) == nil
+        @check_advisor[:advisor] = 'error'
+        render 'edit'
+      elsif co_advisor_to_id(user_params) == nil
+        @check_advisor[:co_advisor] = 'error'
+        render 'edit'
+      elsif official_advisor_to_id(user_params) == nil
+        @check_advisor[:official_advisor] = 'error'
+        render 'edit'
+      else
+        @user = User.update(@user.id, advisor_to_id(co_advisor_to_id(official_advisor_to_id(user_params))))
+        flash[:notice] = 'Successfully saved.'
+        redirect_to admin_showall_path
+      end
+    else
+      @user = User.update(@user.id, user_params)
+      flash[:notice] = 'Successfully saved.'
+      redirect_to admin_showall_path
+    end
   end
 
   def detail
     @user = User.find params[:id]
+    @user_advisor_name = User.find_by(jobid: @user.advisor).name
+    @user_co_advisor_name = User.find_by(jobid: @user.co_advisor).name
+    @user_official_advisor_name = User.find_by(jobid: @user.official_advisor).name
   end
 
   private
@@ -56,7 +81,7 @@ class AdminController < ApplicationController
   end
 
   def advisor_to_id(user_params)
-    @user_advisor = User.find_by name: user_params[:advisor]
+    @user_advisor = User.find_by name: user_params[:advisor], job: 'Teacher'
     if @user_advisor == nil
       return nil
     end
@@ -67,7 +92,7 @@ class AdminController < ApplicationController
   end
 
   def co_advisor_to_id(user_params)
-    @user_co_advisor = User.find_by name: user_params[:co_advisor]
+    @user_co_advisor = User.find_by name: user_params[:co_advisor], job: 'Teacher'
     if @user_co_advisor == nil
       return nil
     end
@@ -78,7 +103,7 @@ class AdminController < ApplicationController
   end
 
   def official_advisor_to_id(user_params)
-    @user_official_advisor = User.find_by name: user_params[:official_advisor]
+    @user_official_advisor = User.find_by name: user_params[:official_advisor], job: 'Teacher'
     if @user_official_advisor == nil
       return nil
     end
