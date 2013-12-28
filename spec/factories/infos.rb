2cc4a705a805fdce99c311dd88684eddc086e9f2
Da Wei <xichuanglian@gmail.com>
2013-12-28 16:30:57 +0800

    added spec for classes_taken_infos_controller

--- spec/factories/infos.rb
+++ spec/factories/infos.rb
@@ -5,4 +5,14 @@ FactoryGirl.define do
     details 'none'
     remark 'none'
   end
+
+  factory :classes_taken_info do
+    course_number 'abc123'
+    name 'test course'
+    instructor 'Foo Bar'
+    time 'sometime'
+    credits '10'
+    score '100'
+    remark 'none'
+  end
 end
